a6bd47f8318d552611799a417a649a1a02bb15bf
Fakpo <facundomaidana105@gmail.com>
2026-07-05 15:42:25 -0300

    comit arranque modularizacion

--- Backend/index.js
+++ Backend/index.js
@@ -6,10 +6,17 @@ import cors from "cors";
 import { Connection } from "mysql2";
 import loggerURL from './src/api/middlewares/middlewares.js';
 
+import path, { dirname } from "path"; // Para armar las rutas de archivos
+import { fileURLToPath } from "url";
+import { error } from "console";
 
 
 const app= express();
 const port = enviroments.port;
+
+//Conexion front
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 // middlewares
@@ -19,10 +26,55 @@ app.use(loggerURL);
 
 app.use(express.json());
 
-/*
-elemento.addEventListener("keydown",(event) => {
-    console.log("la tecla pulsada fue", event.key)});
-*/
+const categoriasValidas = ["Hielo","Bebida"];
+const validateProduct = (req,res,next) =>  {
+    const {nombre, categoria, precio} = req.body;
+
+    const errores = {};
+
+    if (typeof name !== "string" || name.trim().length < 2) {
+        errores.push("El nombre debe tener almenos 2 caracteres");
+    };
+
+    if (typeof price !== "number" || name <= 0) {
+        errores.push("el precio no puede ser menor a 0");
+    };
+
+    // multer para imagenes
+
+    if (!categoriasValidas.includes(categoria)) {
+        errores.push("Categoria Invalida");
+    };
+
+    if (errores.leght() > 0){
+        return res.status(400).json({
+            message:"Datos invalidos", errores
+        });
+    }
+};
+
+
+
+const validateId = (req,res,next) => {
+    const { id } = req.params;
+
+    if (!/^\d+$/.test(id)) {
+        return res.status(400).json({
+            error: "El ID debe ser un numero entero positivo"
+        });
+    }
+
+    const parseID = parseInt(id, 10);
+
+    if (parseID=== 0){
+        return res.status(400).json({
+            error: "El id debe ser mayor a 0"
+        });
+    }
+    req.id = parseID;
+};
+
+app.use(express.static(path.join(__dirname, "..", "Frontend")));
 // Endpoints 
 
 //get
@@ -32,28 +84,45 @@ app.get("/" , (req,res) => {
 app.get("/api/products", async(req,res) => {
     try {
         // El front tiene q apuntar a esta url y leer ese json
-        const sql = "SELECT * FROM productos";
+        const sql = "SELECT id, nombre, imagen, precio, estadoActivo FROM productos";
+
         const [rows] = await conecction.query(sql);
         console.log(rows);
         
+        if (rows.length === 0) {
+            return res.status(404).json({
+                message: "No se encontraron Productos"
+            });
+        }
         res.status(200).json({
+            total: rows.length(),
             payload: rows
         });
 
     } catch (error){
         console.log("Error obteniendo Prductos", error);
+
+        res.status(500).json({
+            message: "Error interno al obtener los productos"
+        });
     };
 });
 
-
-app.get("/api/products/:id", async(req,res) => {
+//get by id
+app.get("/api/products/:id", validateId, async(req,res) => {
     try{
-        const { id } = req.params; // SAME const id = req.params.id;
+        // const { id } = req.params; // SAME const id = req.params.id;
 
-        const sql = "SELECT * FROM productos where productos.id = ?";
-        const [rows] = await conecction.query(sql, [id]);
+        const sql = "SELECT id, nombre, imagen, precio, estadoActivo FROM productos where productos.id = ?";
+        const [rows] = await conecction.query(sql, [req.id]);
 
-        console.log(rows);
+        // console.log(rows);
+
+        if (rows.length === 0) {
+            return res.status(404).json({
+                message: `No se encontraron Productos con id: ${req.id}`
+            });
+        } 
 
         res.status(200).json({
             payload: rows[0]
@@ -61,39 +130,63 @@ app.get("/api/products/:id", async(req,res) => {
 
     }catch(error){
         console.log("Error Obteniendo producto por id", error.message );
+
+        res.status(500).json({
+            error: "Error inerno al obtener prodcuto con id"
+        });
     }
 });
 
 // POST
-app.post("/api/products", async (req, res) => {
+app.post("/api/products", validateProduct, async (req, res) => {
     try {
         console.log(req.body);
         
-        const {nombre , imagen , categoria, precio} = req.body;
+        const {nombre , imagen , categoria, precio, estadoActivo} = req.body;
 
-        const sql = `INSERT INTO productos (nombre, imagen, categoria, precio) VALUES ( ? ,? , ? , ? )`;
+        if (!name || !image || !category || !price) {
+            return res.status(400).json({
+                message: "datos invalidos, asegurate de incluir todas las columnas"
+            });
+        }
+
+        const nombreLimpio = nombre.trim();
+
+        const sql = `INSERT INTO productos (nombreLimpio, imagen, categoria, precio, estadoActivo) VALUES ( ? ,? , ? , ? ,?)`;
         
-        await conecction.query(sql, [nombre , imagen , categoria, precio]);
+        const { rows } = await conecction.query(sql, [nombre , imagen , categoria, precio, estadoActivo]);
 
-        res.status(200).json({
-            message: "Producto creado con exito"
+        res.status(201).json({
+            message: "Producto creado con exito",
+            productId: rows.instertId
         });
     } catch (error) {
-        console.log("" , error);
+        console.log("Error al conetar con la base de datos" , error);
         
     }
 });
 
 
 // DELETE   ( HACER BAJA LOGICA Y PONERLE ACTIVE EN 0)
-app.delete("/api/products/:id", async(req,res) => {
-    const { id } = req.params;
+app.delete("/api/products/:id",validateId, async(req,res) => {
+    // const { id } = req.params;
+    try {
+        const sql = "DELETE FROM productos WHERE id = ? "
+        await conecction.query(sql, [req.id]);
 
-    await conecction.query(sql, [id]);
-    const sql = "DELETE FROM productos WHERE id = ?"
-    res.status(200).json({
-        message: `producto con id: ${id} Eliminado`
-    });
+        res.status(200).json({
+            message: `producto con id: ${req.id} Eliminado`
+        });
+        
+        // res.status(204).json({}); Por si quisiera pasarlo son body
+
+    } catch (error) {
+        console.log("ERror al intentar conectar a la base de datos", error);
+        
+        res.status(500).json({
+            message: "error interno de el servidor al intentar agarrar el Id"
+        });
+    }
 });
 
 // Update products ( Put)
@@ -102,12 +195,23 @@ app.put("/api/products", async(req,res) =>{
         
         const {id,nombre , imagen , categoria, precio, estadoActivo } = req.body;
 
+        if (!name || !image || !category || !price) {
+            return res.status(400).json({
+                message: "datos invalidos, asegurate de incluir todas las columnas"
+            });
+        }
+
         const sql = "UPDATE productos SET nombre = ?, imagen = ?, categoria = ?, precio = ?, estadoActivo = ? WHERE id = ?"
 
-        await conecction.query(sql,[nombre,imagen,categoria,precio,,estadoActivo,id]);
+        const { result } = await conecction.query(sql,[nombre,imagen,categoria,precio,,estadoActivo,id]);
 
+        if (result.affectedRows === 0 ) {
+            return res.status(404).json({
+                message: "No se actualizo nada"
+            });
+        }
         res.status(200).json({
-            message: "Producto actualizado correctamente"
+            message: "Producto con id actualizado correctamente"
         });
 
         
