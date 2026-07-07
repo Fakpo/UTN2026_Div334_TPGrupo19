3bf8f68c69179f63905db4792b8bc5fd4dca479f
Fackpo <facundomaidana105@gmail.com>
2026-07-07 00:40:24 -0300

    arranque ejs

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import enviroments from "./src/api/config/enviroments.js";
 import cors from "cors";
 import {loggerURL} from './src/api/middlewares/middlewares.js';
-import path, { dirname } from "path"; // Para armar las rutas de archivos
+import path, { dirname , join } from "path"; // Para armar las rutas de archivos
 import { fileURLToPath } from "url";
 import { productRoutes } from "./src/api/routes/index.js";
 
@@ -13,6 +13,9 @@ const port = enviroments.port;
 //Conexion front
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views" , join(__dirname, "../Backend/src/views"))
 
 
 // middlewares
@@ -31,6 +34,10 @@ app.get("/" , (req,res) => {
 });
 
 app.use("/api/products", productRoutes )
+
+app.use( "/dashboard" , (req,res) => {
+    res.render("index")
+});
 //listener
 app.listen(port, () => {
     console.log("servidor corriendo en el puerto", port);
